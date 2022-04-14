5f66dc7a4d10447d3946268c70f06aa39d0340f4
minjong_ha <hmjong1128@gmail.com>
2022-04-14 09:17:52 +0900

    DONE: fix UI; TODO: add posts

--- docs/_posts/2022-01-19-final-university-schedule.markdown
+++
@@ -1,9 +0,0 @@
----
-layout: posts
-title: "I finally submit my thesis"
-author: Minjong Ha
-date: 2022-01-19 16:33:00 +0900
----
-
-  I finally submit my thesis to the libray in Ajou university.
-  This is my final schedule for graduation and now I need to celebrate.
--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -4,10 +4,4 @@ var store = [{
         "tags": [],
         "url": "/2022/01/14/welcome-to-jekyll.html",
         "teaser": null
-      },{
-        "title": "I finally submit my thesis",
-        "excerpt":"I finally submit my thesis to the libray in Ajou university.   This is my final schedule for graduation and now I need to celebrate.  ","categories": [],
-        "tags": [],
-        "url": "/2022/01/19/final-university-schedule.html",
-        "teaser": null
       }]
