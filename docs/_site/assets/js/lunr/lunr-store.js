d177fe8de4ac82bc7700824f0c5da92f83125b26
minjong_ha <hmjong1128@gmail.com>
2022-05-03 17:05:52 +0900

    fix index.html

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": [],
         "url": "/2022/04/25/host-guest-communication.html",
         "teaser": null
+      },{
+        "title": "Lightweight Persistence Centric System",
+        "excerpt":"Non-volatile computing demo (LightPC) English version LightPC Presents a Resilient System Using Only Non-Volatile Memory   “A KAIST research team has developed hardware and software technology that ensures both data and execution persistence.”   This is very interesting research! At the time I graduated from the University with the research about the non-volatile memory, also called persistent memory, it was not a major trend in academia as far as I know. Compared to the costs and efforts of applying non-volatile memory from the volatile-memory, the performance improvement did not look attractive. Thus, the companies preferred to increase the memory physically, and the researchers start to study about the “CXL memory” instead.   However, the researchers in CAMEL lab (KAIST) implement a integrated persistence centric system with the Intel OptaneDC Persistent Memory!   The paper, “LightPC: Hardware and Software Co-Design for Energy-Efficient Full System Persistence)”, will be presented in ISCA’22 (International Symposium on Computer Architecture). As soon as the paper is announced, I will read and analyze the LightPC! and update this posts!  ","categories": [],
+        "tags": [],
+        "url": "/2022/04/27/Lightweight_Persistence_Centric_System.html",
+        "teaser": null
       }]
